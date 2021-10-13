3d2211ed8e676279aa6d135bd60fe67628ce4fa6
NTaylorOX <niall.taylor@psych.ox.ac.uk>
2021-10-13 16:50:49 +0100

    torch load model via state dict

--- clinical-longformer/classifier_pipeline/bert_interpretability.py
+++ clinical-longformer/classifier_pipeline/bert_interpretability.py
@@ -19,7 +19,7 @@ from pytorch_lightning.loggers import LightningLoggerBase, TensorBoardLogger
 from torchnlp.random import set_seed
 
 
-def main(hparams) -> None:
+def main(hparams, model_dir) -> None:
     """
     Main training routine specific for this project
     :param hparams:
@@ -29,9 +29,19 @@ def main(hparams) -> None:
     # 1 INIT LIGHTNING MODEL AND DATA
     # ------------------------
 
+    checkpoint = torch.load(model_dir)
+    print(f"Checkpoint hyper parameters: {checkpoint['hyper_parameters']}")
+    print(f"Checkpoint state_dict: {checkpoint.keys()}")
     model = Classifier(hparams)
-    print("model is:", model)
 
+    #below isn't working for some reason - it ends up having hparams issues
+    # model.load_from_checkpoint(model_dir)   
+    model.load_state_dict(checkpoint['state_dict'])
+    print(f"model is: {model}")
+
+
+    
+    
 
 if __name__ == "__main__":
     # ------------------------
@@ -114,4 +124,6 @@ if __name__ == "__main__":
     # ---------------------
     # RUN TRAINING
     # ---------------------
-    main(hparams)
+    main(hparams,
+        model_dir = "/home/niallt/NLP_DPhil/NLP_Mimic_only/clinical-longformer/experiments/emilyalsentzer/Bio_ClinicalBERT/version_20-09-2021--11-08-38/checkpoints/epoch=3-step=2871.ckpt"
+        )
