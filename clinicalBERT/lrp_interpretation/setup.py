fd2bafe0557195ef4476ec671a1501de942b5186
NTaylorOX <niall.taylor@psych.ox.ac.uk>
2022-01-25 16:00:48 +0000

    Cleaning up

--- clinicalBERT/lrp_interpretation/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-
-setup(name="interpret-nlp",
-      version="0.1",
-      packages=["interpret_nlp", "interpret_nlp.attribution",
-                "interpret_nlp.modules", "interpret_nlp.visualization"])
