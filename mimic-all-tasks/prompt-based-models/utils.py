b63694fd58928ea6bb49f17b82496cc86067ac44
NTaylorOX <niall.taylor@psych.ox.ac.uk>
2022-02-24 09:40:55 +0000

    added mimic mortality prompt data processor

--- mimic-all-tasks/prompt-based-models/utils.py
+++ mimic-all-tasks/prompt-based-models/utils.py
@@ -177,10 +177,11 @@ class Mimic_ICD9_Triage_Processor(DataProcessor):
 
             textfile = open(f"{class_labels_save_dir}/labels.txt", "w")
 
-            for element in class_labels:
+            for element in class_labels[:-1]:
 
                 textfile.write(element + "\n")
-
+                # now write the last item to the file
+            textfile.write(class_labels[-1])
             textfile.close() 
 
         return examples
@@ -205,3 +206,93 @@ class Mimic_ICD9_Triage_Processor(DataProcessor):
         return class_labels
 
 
+class Mimic_Mortality_Processor(DataProcessor):
+
+
+    '''
+    Function to convert mimic mortality prediction dataset from the clinical outcomes paper: https://aclanthology.org/2021.eacl-main.75/
+    
+    to a open prompt ready dataset. 
+    
+    We also instantiate a LabelEncoder() class which is fitted to the given dataset. Fortunately it appears
+    to create the same mapping for each set, given each set contains all classes.    
+    
+    '''
+    # TODO Test needed
+    def __init__(self):
+        super().__init__()        
+
+    def get_examples(self, data_dir, mode = "train", label_encoder = None,
+                     generate_class_labels = False, class_labels_save_dir = "./scripts/mimic_mortality/"):
+
+        path = f"{data_dir}/{mode}.csv"
+        print(f"loading {mode} data")
+        print(f"data path provided was: {path}")
+        examples = []
+        df = pd.read_csv(path)
+
+        # map the binary classification label to a new string class label
+        df["label"] = df["hospital_expire_flag"].map({0:"alive",1:"deceased"})
+        
+        # need to either initializer and fit the label encoder if not provided
+        if label_encoder is None:
+            self.label_encoder = LabelEncoder(np.unique(df["label"]).tolist())
+        else: 
+            print("we were given a label encoder")
+            self.label_encoder = label_encoder
+
+        
+        for idx, row in tqdm(df.iterrows()):
+#             print(row)
+            body, label = row['text'],row['label']
+            label = self.label_encoder.encode(label)
+#             print(f"body : {body}")
+#             print(f"label: {label}")
+#             print(f"labels original: {self.label_encoder.index_to_token[label]}")
+            
+            text_a = body.replace('\\', ' ')
+
+            example = InputExample(
+                guid=str(idx), text_a=text_a, label=int(label))
+            examples.append(example)
+            
+        logger.info(f"Returning {len(examples)} samples!") 
+
+#         now we want to return a list of the non-encoded labels based on the fitted label encoder
+        if generate_class_labels:
+        
+            if not os.path.exists(class_labels_save_dir):
+                os.makedirs(class_labels_save_dir)
+            logger.info(f"Saving class labels to: {class_labels_save_dir}")
+            class_labels = self.generate_class_labels()
+            # write these to files as the classes for prompt learning pipeline           
+
+            textfile = open(f"{class_labels_save_dir}/labels.txt", "w")
+
+            for element in class_labels[:-1]:
+
+                textfile.write(element + "\n")
+            # now write the last item to the file
+            textfile.write(class_labels[-1])
+            textfile.close() 
+
+        return examples
+
+    def generate_class_labels(self):
+        # now we want to return a list of the non-encoded labels based on the fitted label encoder
+        try:
+            return list(self.label_encoder.tokens.keys())
+        except:
+            print("No class labels as haven't fitted any data yet. Run get_examples first!")
+            raise NotImplementedError
+
+    
+    def load_class_labels(self, file_path = "./scripts/mimic_mortality/labels.txt"):
+        # function to load pre-generated class labels
+        # returns list of class labels
+
+        text_file = open(f"{file_path}", "r")
+
+        class_labels = text_file.read().split("\n")
+
+        return class_labels
